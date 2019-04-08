b0747b9bff52bb2195d6d9fa243a0c2b9b63d1b0
angleless <sjydgz@aliyun.com>
2019-04-08 13:47:38 +0800

    day1

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <!-- 轮播图 -->
+    <mt-swipe :auto="2000">
+      <mt-swipe-item class="slide1">1</mt-swipe-item>
+      <mt-swipe-item class="slide2">2</mt-swipe-item>
+      <mt-swipe-item class="slide3">3</mt-swipe-item>
+    </mt-swipe>
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt="">
+          <div class="mui-media-body">Home</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt="">
+          <div class="mui-media-body">Email</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt="">
+          <div class="mui-media-body">Chat</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu4.png" alt="">
+          <div class="mui-media-body">location</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt="">
+          <div class="mui-media-body">Search</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu6.png" alt="">
+          <div class="mui-media-body">Phone</div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+export default {
+  data() {
+    return {
+      lunbotuList: []
+    }
+  },
+  created() {
+    // this.getLunbotu();
+  },
+  methods: {
+    // getLunbotu() {
+    //   this.$http.get("http://vue.studyit.io/api/getlunbo").then(result => {
+    //     // console.log(result.body)
+    //     if (result.body.status === 0) {
+    //       this.lunbotuList = result.body.message;
+    //     } else {
+    //       Toast("加载失败");
+    //     }
+    //   })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mint-swipe {
+  height: 200px;
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: red;
+    }
+    &:nth-child(2) {
+      background-color: blue;
+    }
+    &:nth-child(3) {
+      background-color: green;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+
+.mui-table-view.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border: none;
+  .mui-table-view-cell {
+    border: none;
+  }
+  img {
+    width: 60px;
+    height: 60px;
+  }
+  .mui-media-body {
+    font-size: 13px;
+  }
+}
+</style>
